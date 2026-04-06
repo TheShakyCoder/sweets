b082b7ed8d11097ea08596c1dfe4a1f8e89c9ad4
Sharif Khan <>
2026-04-06 09:22:38 +0100

    installed inertia

---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+const port = 5173
+const origin = `${process.env.DDEV_PRIMARY_URL}:${port}`
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: 'resources/js/app.js',
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+    server: {
+        cors: true,
+        host: "0.0.0.0",
+        port: port,
+        strictPort: true,
+        origin: origin,
+        hmr: {
+            host: process.env.DDEV_HOSTNAME,
+            protocol: 'wss'
+        }
+    },
+});
